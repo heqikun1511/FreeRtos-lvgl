24caefe2268723cb0c7dd90ba10d9ab86b45f9a7
heqikun1511 <3669308367@qq.com>
2026-05-04 16:13:39 +0800

    实现控制速度案例

--- src/freertos_main.c
+++ src/freertos_main.c
@@ -15,6 +15,22 @@
 
 #include "hal/hal.h"
 #include <stdio.h>
+//部件定义
+static lv_obj_t * label_speed;
+static lv_obj_t * btn_up;
+static lv_obj_t * btn_down;
+static lv_obj_t * btn_stop;
+static int32_t speed_val=0;
+
+static int32_t screen_width(void)
+{
+    return lv_display_get_horizontal_resolution(lv_display_get_default());
+}
+
+static int32_t screen_height(void)
+{
+    return lv_display_get_vertical_resolution(lv_display_get_default());
+}
 
 // ........................................................................................................
 /**
@@ -44,6 +60,82 @@ void vApplicationMallocFailedHook(void)
  */
 void vApplicationIdleHook(void) {}
 
+static void lv_example_label(void)//速度提示
+{   //根据屏幕宽度选择合适的字体
+    // if(screen_width()<=320){
+    //     font=&lv_font_montserrat_10;
+    // }
+    // else if(screen_width()<=480){
+    //     font=&lv_font_montserrat_14;
+    // }
+    // else {
+    //     font=&lv_font_montserrat_20;
+    // }
+    label_speed=lv_label_create(lv_scr_act());
+    lv_obj_set_style_text_font(label_speed, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_label_set_text(label_speed, "Speed: 0 RPM");
+    lv_obj_align(label_speed, LV_ALIGN_TOP_MID, 0, screen_height()/12);
+
+}
+static void event_btn_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code==LV_EVENT_CLICKED){
+       lv_obj_t * btn = lv_event_get_target(e);
+       if(btn==btn_up){
+           speed_val+=10;
+    }
+         else if(btn==btn_down){
+              speed_val-=10;
+         }
+         else if(btn==btn_stop){
+              speed_val=0;
+         }
+         char buf[20];
+         sprintf(buf, "Speed: %d RPM", speed_val);
+         lv_label_set_text(label_speed, buf);
+}
+}
+static void lv_example_btnup(void)//加速按钮
+{
+    btn_up=lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn_up, screen_width()/4, screen_height()/6);
+    lv_obj_align(btn_up, LV_ALIGN_CENTER, -screen_width()/3, 0);
+    lv_obj_add_event_cb(btn_up, event_btn_cb, LV_EVENT_CLICKED, NULL);
+    lv_obj_t * label = lv_label_create(btn_up);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_label_set_text(label, "SPEED UP");
+    lv_obj_set_align(label, LV_ALIGN_CENTER);
+
+}
+static void lv_example_btndown(void)//减速按钮
+{
+    btn_down=lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn_down, screen_width()/4, screen_height()/6);
+    lv_obj_align(btn_down, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_event_cb(btn_down, event_btn_cb, LV_EVENT_CLICKED, NULL);
+    lv_obj_t * label = lv_label_create(btn_down);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_label_set_text(label, "SPEED DOWN");
+    lv_obj_set_align(label, LV_ALIGN_CENTER);
+
+}
+
+static void lv_example_btn_stop(void)//停止按钮
+{
+    btn_stop=lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn_stop, screen_width()/4, screen_height()/6);
+    lv_obj_align(btn_stop, LV_ALIGN_CENTER, screen_width()/3, 0);
+    lv_obj_set_style_bg_color(btn_stop, lv_color_hex(0xFF0000), LV_PART_MAIN | LV_STATE_DEFAULT);//默认状态为红色
+    lv_obj_set_style_bg_color(btn_stop, lv_color_hex(0xCC0000), LV_PART_MAIN | LV_STATE_PRESSED);//按下时变暗
+
+    lv_obj_add_event_cb(btn_stop, event_btn_cb, LV_EVENT_CLICKED, NULL);
+    lv_obj_t * label = lv_label_create(btn_stop);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_label_set_text(label, "STOP");
+    lv_obj_set_align(label, LV_ALIGN_CENTER);
+
+}
 // ........................................................................................................
 /**
  * @brief   Stack overflow hook
@@ -93,13 +185,13 @@ static void event_cb(lv_event_t * e)
 lv_obj_t * obj2;
 lv_obj_t * obj1;
 
-void create_hello_world_screen()
+void create_creen()
 {
- lv_obj_t *btn= lv_btn_create(lv_scr_act());
- lv_obj_set_size(btn, 120, 50);
- lv_obj_set_align(btn, LV_ALIGN_CENTER);
- lv_obj_add_flag(btn,LV_OBJ_FLAG_CHECKABLE);
- lv_obj_add_event_cb(btn,event_cb,LV_EVENT_VALUE_CHANGED,NULL);
+    lv_example_label();
+    lv_example_btnup();
+    lv_example_btndown();
+    lv_example_btn_stop();
+
 }
 
 // ........................................................................................................
@@ -121,7 +213,7 @@ void lvgl_task(void *pvParameters)
     /*Initialize the HAL (display, input devices, tick) for LVGL*/
     sdl_hal_init(320, 480);
     /* Show simple hello world screen */
-    create_hello_world_screen();
+    create_creen();
 
     while (true){
         lv_timer_handler(); /* Handle LVGL tasks */
